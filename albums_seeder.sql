324ce0b3e74bf7f88a0edc37ae97aaed07851e49
Michael Coyle <mcoyle74@gmail.com>
2016-07-25 16:05:15 -0500

    Created sql script to populate albums table with data from wikipedia: https://en.wikipedia.org/wiki/List_of_best-selling_albums.

---
+++ albums_seeder.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+INSERT INTO albums (artist, name, sales)
+	VALUES('Michael Jackson', 'Thriller', 45.4),
+	('Eagles', 'Their Greatest Hits', 32.2),
+	('The Beatles', 'Sgt. Pepper\'s Lonely Hearts Club Band', 45.4),
+	('Pink Floyd', 'The Dark Side of the Moon', 22.7),
+	('Nirvana', 'Nevermind', 16.7),
+	('AC/DC', 'Back in Black', 25.9),
+	('Whitney Houston', 'The Bodyguard', 27.4),
+	('Meat Loaf', 'Bat Out of Hell', 20.6);

3f0add22284c363347ce1d5154ee833a1b50a85e
Michael Coyle <mcoyle74@gmail.com>
2016-07-25 16:44:45 -0500

    Created sql script that asks for: The name of all albums by Pink Floyd, The year Sgt. Pepper's Lonely Hearts Club Band was released, The genre for Nevermind, Which albums were released in the 1990s, Which albums had less than 20 million certified sales and All the albums in the rock genre. Is this all the rock albums in the table?

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,10 +1,10 @@
 USE codeup_test_db;
-INSERT INTO albums (artist, name, sales)
-	VALUES('Michael Jackson', 'Thriller', 45.4),
-	('Eagles', 'Their Greatest Hits', 32.2),
-	('The Beatles', 'Sgt. Pepper\'s Lonely Hearts Club Band', 45.4),
-	('Pink Floyd', 'The Dark Side of the Moon', 22.7),
-	('Nirvana', 'Nevermind', 16.7),
-	('AC/DC', 'Back in Black', 25.9),
-	('Whitney Houston', 'The Bodyguard', 27.4),
-	('Meat Loaf', 'Bat Out of Hell', 20.6);
+INSERT INTO albums (artist, name, release_date, sales, genre)
+	VALUES('Michael Jackson', 'Thriller', 1982, 45.4, 'pop, rock, R&B'),
+	('Eagles', 'Their Greatest Hits', 1976, 32.2, 'rock'),
+	('The Beatles', 'Sgt. Pepper\'s Lonely Hearts Club Band', 1967, 45.4, 'rock'),
+	('Pink Floyd', 'The Dark Side of the Moon', 1973, 22.7, 'progessive rock'),
+	('Nirvana', 'Nevermind', 1991, 16.7, 'Grunge, alternative rock'),
+	('AC/DC', 'Back in Black', 1980, 25.9, 'hard rock'),
+	('Whitney Houston', 'The Bodyguard', 1992, 27.4, 'soundtrack, R&B, pop'),
+	('Meat Loaf', 'Bat Out of Hell', 1977, 20.6, 'hard rock, progessive rock');

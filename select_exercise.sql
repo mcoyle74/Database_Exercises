64d084338355d8fb105e712e5fb1f6ee2512e095
Michael Coyle <mcoyle74@gmail.com>
2016-07-26 09:24:29 -0500

    Simplified caption text.

--- select_exercise.sql
+++ select_exercise.sql
@@ -1,7 +1,7 @@
 USE codeup_test_db;
-SELECT 'The name of all albums by Pink Floyd' AS caption, name FROM albums WHERE artist = 'Pink Floyd';
-SELECT 'The year Sgt. Pepper\'s Lonely Hearts Club Band was released' AS caption, release_date FROM albums WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
-SELECT 'The genre for Nevermind' AS caption, genre FROM albums WHERE name = 'Nevermind';
-SELECT 'Which albums were released in the 1990s' AS caption, name FROM albums WHERE release_date < 2000 AND release_date > 1989;
-SELECT 'Which albums had less than 20 million certified sales' AS caption, name FROM albums WHERE sales < 20;
-SELECT 'All the albums in the rock genre. Is this all the rock albums in the table?' AS caption, name FROM albums WHERE genre = 'rock';
+SELECT 'All albums by Pink Floyd' AS caption, name FROM albums WHERE artist = 'Pink Floyd';
+SELECT 'Year Sgt. Pepper\'s Lonely Hearts Club Band was released' AS caption, release_date FROM albums WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
+SELECT 'Nevermind genre' AS caption, genre FROM albums WHERE name = 'Nevermind';
+SELECT 'Albums released in the 1990s' AS caption, name FROM albums WHERE release_date < 2000 AND release_date > 1989;
+SELECT 'Albums with less than 20 million certified sales' AS caption, name FROM albums WHERE sales < 20;
+SELECT 'All albums in the rock genre. Is this all the rock albums in the table?' AS caption, name FROM albums WHERE genre = 'rock';

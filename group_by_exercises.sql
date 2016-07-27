0d8491730b5652cee6d7df7eae56cc0a30461af3
Michael Coyle <mcoyle74@gmail.com>
2016-07-27 14:55:41 -0500

    Wrote query to find DISTINCT last names that contain a 'q' but not 'qu'.

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -4,3 +4,6 @@ GROUP BY title ASC;
 SELECT first_name, last_name FROM employees
 WHERE last_name LIKE 'E%' AND last_name LIKE '%e'
 GROUP BY first_name, last_name;
+
+SELECT DISTINCT last_name FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';

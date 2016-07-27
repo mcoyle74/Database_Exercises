811f46de82a1f7b703b5372c3004e21799066334
Michael Coyle <mcoyle74@gmail.com>
2016-07-27 15:37:26 -0500

    Update last query to output 10th batch of results limited to 5 rows

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -2,3 +2,10 @@ SELECT last_name
 FROM employees
 GROUP BY last_name DESC
 LIMIT 10;
+
+SELECT * 
+FROM employees
+WHERE birth_date LIKE '%12-25'
+	AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
+ORDER BY birth_date, hire_date DESC
+LIMIT 5 OFFSET 45;

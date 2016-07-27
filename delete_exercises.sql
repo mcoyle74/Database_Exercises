6415cf8014b7161e679358b27e09e05ecb337843
Michael Coyle <mcoyle74@gmail.com>
2016-07-27 09:41:39 -0500

    Changed all SELECT to DELETE

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,4 +1,4 @@
 USE codeup_test_db;
-SELECT 'Albums released after 1991' AS 'Date', name, release_date FROM albums WHERE release_date > 1991;
-SELECT 'Disco' AS 'Genre', name, genre FROM albums WHERE genre = 'disco';
-SELECT 'Whitney Houston albums' AS 'Artist', name, artist FROM albums WHERE artist = 'Whitney Houston';
+DELETE FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE genre = 'disco';
+DELETE FROM albums WHERE artist = 'Whitney Houston';

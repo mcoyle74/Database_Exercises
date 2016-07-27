98f9119bbe29a373009d77b41a6f94681ecfea3a
Michael Coyle <mcoyle74@gmail.com>
2016-07-27 11:49:10 -0500

    Modified query to select all employees whose last name starts or ends with an 'e'

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,5 +1,5 @@
 SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
-SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
 SELECT * FROM employees WHERE birth_date = '1990-01-01' AND '1999-12-31';
 SELECT * FROM employees WHERE birth_date LIKE '%12-25';
 SELECT * FROM employees WHERE last_name LIKE '%q%';

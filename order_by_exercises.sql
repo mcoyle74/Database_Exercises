757f2af217ab871939fab59630876bfd3c4c7c06
Michael Coyle <mcoyle74@gmail.com>
2016-07-27 13:23:07 -0500

    Changed ORDER BY to DESC.

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -4,11 +4,11 @@ ORDER BY last_name, first_name;
 
 SELECT * FROM employees
 WHERE last_name LIKE 'E%' OR last_name LIKE '%e'
-ORDER BY emp_no;
+ORDER BY emp_no DESC;
 
 SELECT * FROM employees
 WHERE last_name LIKE 'E%' AND last_name LIKE '%e'
-ORDER BY emp_no;
+ORDER BY emp_no DESC;
 
 SELECT * FROM employees
 WHERE birth_date LIKE '%12-25' AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31');

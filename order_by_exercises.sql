bc3f921bdb399db7fedff6feeb4e7e9859c51a68
Michael Coyle <mcoyle74@gmail.com>
2016-07-27 12:57:03 -0500

    Updated first query to order by first name and then last name.

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,6 +1,6 @@
 SELECT * FROM employees
 WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
-ORDER BY first_name;
+ORDER BY first_name, last_name;
 
 SELECT * FROM employees
 WHERE last_name LIKE 'E%' OR last_name LIKE '%e';

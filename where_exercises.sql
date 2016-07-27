33329c72488b16715f7d1887d104ee7e59c5eb2e
Michael Coyle <mcoyle74@gmail.com>
2016-07-27 09:49:46 -0500

    Added query to find all employees with a 'q' in their last name.

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,3 +2,4 @@ SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 SELECT * FROM employees WHERE last_name LIKE 'E%';
 SELECT * FROM employees WHERE birth_date = '1990-01-01' AND '1999-12-31';
 SELECT * FROM employees WHERE birth_date LIKE '%12-25';
+SELECT * FROM employees WHERE last_name LIKE '%q%';

b126090995542967176e71c84d74d8fd3ebbbaaa
Michael Coyle <mcoyle74@gmail.com>
2016-07-27 09:46:49 -0500

    Completed query to find all employees hired during 1990s.

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,2 +1,3 @@
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';

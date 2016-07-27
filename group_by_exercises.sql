f70e1da1e3d8c5c090b794875a13c0256e5cd636
Michael Coyle <mcoyle74@gmail.com>
2016-07-27 14:48:37 -0500

    Updated query to find last names that start and end with an 'e' so that it finds only unique last names.

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,1 +1,6 @@
-SELECT title FROM titles GROUP BY title ASC;
+SELECT title FROM titles
+GROUP BY title ASC;
+
+SELECT last_name FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%e'
+GROUP BY last_name;

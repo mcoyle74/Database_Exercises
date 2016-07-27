e6258fc8f0f63ccb09ff64c6fd7d09d5c65e83b7
Michael Coyle <mcoyle74@gmail.com>
2016-07-27 14:52:21 -0500

    Updated 'e' last name query to group by unique first and last name combinations.

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,6 +1,6 @@
 SELECT title FROM titles
 GROUP BY title ASC;
 
-SELECT last_name FROM employees
+SELECT first_name, last_name FROM employees
 WHERE last_name LIKE 'E%' AND last_name LIKE '%e'
-GROUP BY last_name;
+GROUP BY first_name, last_name;

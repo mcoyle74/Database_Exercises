489a4dea7cbeb1334e5dffafd20380c2fde4c8e7
Michael Coyle <mcoyle74@gmail.com>
2016-07-27 15:11:25 -0500

    Simplified condition 'WHERE last_name LIKE 'E%' AND last_name LIKE '%e'' to 'WHERE last_name LIKE 'e%e''.

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -2,7 +2,7 @@ SELECT title FROM titles
 GROUP BY title ASC;
 
 SELECT first_name, last_name FROM employees
-WHERE last_name LIKE 'E%' AND last_name LIKE '%e'
+WHERE last_name LIKE 'e%e'
 GROUP BY first_name, last_name;
 
 SELECT DISTINCT last_name FROM employees

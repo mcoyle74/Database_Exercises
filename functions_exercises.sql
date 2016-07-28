12e4cd0ed91bd85f8865ebf3efb6834fceb829c3
Michael Coyle <mcoyle74@gmail.com>
2016-07-27 20:10:29 -0500

    Updated queries for employees whose names start and end with 'E' using CONCAT() to output their full name as a single column the results. Also concatenaed full name of employees born on Christmas and hired in the 90s and used datediff() to find how many days they have been working at the company.

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,24 +1,30 @@
-SELECT COUNT(*) FROM employees
+SELECT COUNT(*), gender
+	FROM employees
 	WHERE
 		first_name = 'Irena' OR
 		first_name = 'Vidya' OR
 		first_name = 'Maya'
 	GROUP BY gender;
 
-SELECT * FROM employees
-	WHERE last_name LIKE 'E%' OR last_name LIKE '%e'
+SELECT CONCAT(first_name, ' ', last_name) AS 'full_name'
+	FROM employees
+	WHERE last_name LIKE 'e%' OR last_name LIKE '%e'
 	ORDER BY emp_no DESC;
 
-SELECT * FROM employees
-	WHERE last_name LIKE 'E%' AND last_name LIKE '%e'
+SELECT CONCAT(first_name, ' ', last_name) AS 'full_name'
+	FROM employees
+	WHERE last_name LIKE 'e%e'
 	ORDER BY emp_no DESC;
 
-SELECT * FROM employees
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS 'full_name', hire_date, datediff(curdate(), hire_date) AS 'tenure'
+	FROM employees
 	WHERE birth_date LIKE '%12-25' AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
-	ORDER BY birth_date, hire_date DESC;
+	ORDER BY birth_date, hire_date ASC;
 
-SELECT * FROM employees
+SELECT *
+	FROM employees
 	WHERE birth_date LIKE '%12-25';
 
-SELECT * FROM employees
+SELECT *
+	FROM employees
 	WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';

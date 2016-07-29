8d12f1470608e9d3c927e007677878482f049bde
Michael Coyle <mcoyle74@gmail.com>
2016-07-29 14:45:38 -0500

    Wrote query to find all the titles held by all employees w/ first name 'Aamod.'

--- sub-queries.sql
+++ sub-queries.sql
@@ -1,7 +1,17 @@
+-- Finds all employees with the same hire date as employee 101010.
 SELECT e.emp_no, CONCAT(e.first_name, ' ', e.last_name) AS 'Employee', e.hire_date
 FROM employees AS e
 WHERE hire_date IN (
 	SELECT hire_date
 	FROM employees
 	WHERE emp_no = 101010
+);
+
+-- Finds all the titles held by all employees w/ first name 'Aamod.'
+SELECT title
+FROM titles
+WHERE emp_no IN (
+	SELECT emp_no
+	FROM employees
+	WHERE first_name = 'Aamod'
 );

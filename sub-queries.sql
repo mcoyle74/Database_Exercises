af2fc0d24d644d8aca0e24a663b96e38a7415e65
Michael Coyle <mcoyle74@gmail.com>
2016-07-29 15:17:14 -0500

    Wrote query to find all department managers that are female.

--- sub-queries.sql
+++ sub-queries.sql
@@ -1,4 +1,4 @@
--- Finds all employees with the same hire date as employee 101010.
+-- Find all employees with the same hire date as employee 101010.
 SELECT e.emp_no, CONCAT(e.first_name, ' ', e.last_name) AS 'Employee', e.hire_date
 FROM employees AS e
 WHERE hire_date IN (
@@ -7,11 +7,35 @@ WHERE hire_date IN (
 	WHERE emp_no = 101010
 );
 
--- Finds all the titles held by all employees w/ first name 'Aamod.'
+-- Find all the titles held by all employees w/ first name 'Aamod.'
 SELECT title
 FROM titles
 WHERE emp_no IN (
 	SELECT emp_no
 	FROM employees
 	WHERE first_name = 'Aamod'
+);
+
+-- Find all the department managers that are female.
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM employees AS e
+WHERE emp_no IN (
+	SELECT emp_no
+	FROM employees
+	WHERE gender = 'F' AND emp_no IN (
+		SELECT emp_no
+		FROM dept_manager
+	)
+);
+
+-- BONUS Find all the department names that have female managers.
+SELECT dept_name
+FROM departments;
+WHERE dept_no IN (
+	SELECT emp_no
+	FROM dept_manager
+	WHERE to_date = '9999-01-01' AND emp_no IN (
+		SELECT emp_no
+		FROM employees
+		WHERE gender = 'F')
 );

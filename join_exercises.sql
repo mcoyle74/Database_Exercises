8bc6340385570402d6b1837f789b729831b23ff4
Michael Coyle <mcoyle74@gmail.com>
2016-07-29 11:40:40 -0500

    Changed LEFT JOIN to RIGHT JOIN on 2nd query. Wrote new query to select count of users per role

--- join_exercises.sql
+++ join_exercises.sql
@@ -7,4 +7,10 @@ SELECT
 	roles.id AS 'Role ID',
 	roles.name AS 'Role'
 	FROM users
-	LEFT JOIN roles ON users.role_id = roles.id;
+	RIGHT JOIN roles ON users.role_id = roles.id;
+
+SELECT roles.name AS 'Role', COUNT(users.name) AS 'Count'
+	FROM users
+	JOIN roles ON users.role_id = roles.id
+	-- LEFT JOIN could be used above to also select users with NULL roles (3).
+	GROUP BY roles.name;
